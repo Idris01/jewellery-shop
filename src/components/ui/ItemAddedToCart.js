8e01636bb51da6ba141cab08ae367fad6206461a
idris01 <idrys01@gmail.com>
2023-04-01 03:40:36 -0700

    put comment inside braces

--- src/components/ui/ItemAddedToCart.js
+++ src/components/ui/ItemAddedToCart.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function ItemAddedToCart() {
 	return (
 		<div>
+			{
 			//todo ItemAddedToCart
+			// This will be a ui component to tells the state of the 
+			// cart whether or not the item is successully added to cart
+			}
 		</div>
 	)
 }
